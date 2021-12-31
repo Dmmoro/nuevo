e6b03177e9abf1f0a6c50c5b0be3d99c16229e1c
datasci <dmartinmor2uoc.edu>
2021-12-31 11:02:41 +0100

    segundo nuevo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+# y ahora pongo otra cosa
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -15,3 +15,5 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print("hola mundo")
+
+
